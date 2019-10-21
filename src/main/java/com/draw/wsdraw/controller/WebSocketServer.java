0522acac164719909e15be24b17d1034639e24de
LiuYuan <ly405653510@gmail.com>
2019-10-21 14:43:03 +0800

    commit

--- src/main/java/com/draw/wsdraw/controller/WebSocketServer.java
+++ src/main/java/com/draw/wsdraw/controller/WebSocketServer.java
@@ -14,22 +14,53 @@ import javax.websocket.server.ServerEndpoint;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @ServerEndpoint("/webSocket/{userId}")
 @Component
 public class WebSocketServer {
 
-    private static ConcurrentHashMap<String, List<WebSocketServer>> webSocketMap = new ConcurrentHashMap<>(3);
+    private static final ConcurrentHashMap<String, List<WebSocketServer>> webSocketMap = new ConcurrentHashMap<>(3);
 
     /**
      * 与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
-    private Session session=null;
+    private Session session = null;
 
     /**
      * 接收userId
      */
     private String userId = null;
+
+    private static synchronized void addSocketServerToMap(WebSocketServer wss) {
+        if (wss != null) {
+            List<WebSocketServer> wssList = webSocketMap.get(wss.userId);
+            if (wssList == null) {
+                wssList = new ArrayList<>(6);
+                webSocketMap.put(wss.userId, wssList);
+            }
+            wssList.add(wss);
+        }
+    }
+
+    /**
+     * 实现服务器主动发送消息
+     */
+    public static void sendMessage(String userId, String message) {
+        if (StrUtil.isBlank(userId)) return;
+        List<WebSocketServer> wssList = webSocketMap.get(userId);
+        for (WebSocketServer item : wssList) {
+            sendMessageBySession(item.session, message);
+        }
+    }
+
+    private static void sendMessageBySession(Session session, String message) {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (Exception e) {
+            log.error("消息发送失败", e);
+        }
+    }
 
     /**
      * 连接建立成功调用的方法
@@ -42,7 +73,7 @@ public class WebSocketServer {
         this.session = session;
         this.userId = userId;
         addSocketServerToMap(this);
-        sendMessageBySession(session,"连接成功");
+        sendMessageBySession(session, "连接成功");
     }
 
     /**
@@ -71,46 +102,18 @@ public class WebSocketServer {
      */
     @OnMessage
     public void onMessage(String message, Session session) {
+//        sendMessage(this.userId, message);
         sendMessageBySession(session,message);
     }
 
     /**
      * 服务异常处理
+     *
      * @param session
      * @param error
      */
     @OnError
     public void onError(Session session, Throwable error) {
-       log.error("websocket服务异常",error);
-    }
-
-    private static synchronized void addSocketServerToMap(WebSocketServer wss) {
-        if (wss != null) {
-            List<WebSocketServer> wssList = webSocketMap.get(wss.userId);
-            if (wssList == null) {
-                wssList = new ArrayList<>(6);
-                webSocketMap.put(wss.userId, wssList);
-            }
-            wssList.add(wss);
-        }
-    }
-
-    /**
-     * 实现服务器主动发送消息
-     */
-    public static void sendMessage(String userId,String message) {
-        if (StrUtil.isBlank(userId)) return;
-        List<WebSocketServer> wssList = webSocketMap.get(userId);
-        for (WebSocketServer item : wssList) {
-            sendMessageBySession(item.session,message);
-        }
-    }
-
-    private static void sendMessageBySession(Session session,String message){
-        try {
-            session.getBasicRemote().sendText(message);
-        } catch (Exception e) {
-           log.error("消息发送失败",e);
-        }
+        log.error("websocket服务异常", error);
     }
 }
